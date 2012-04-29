fffc8e27721fb442107bf1ad41e5b218268ff321
Vaishaal <vaishaal@berkeley.edu>
2012-04-29 02:24:51 -0700

    oauth works

--- tendril_oauth/login.py
+++ tendril_oauth/login.py
@@ -2,5 +2,7 @@ import webbrowser
 from urllib import * 
 
 AUTHORIZE_URL = 'https://dev.tendrilinc.com/oauth/authorize?response_type=code&client_id=a4ca80c12d6fc2612858602edc4d75dd&redirect_uri=http://localhost:1337/auth&scope=\'consumption\'&state=dog'
-def generateOAuthLink(): 
-	webbrowser.open(AUTHORIZE_URL) 
+
+
+
+webbrowser.open(AUTHORIZE_URL) 
--- tendril_oauth/models.py
+++ tendril_oauth/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Data(models.Model):
+	access_token = CharField(max_length=50);
+	token_type = CharField(max_length=50);
+	expires_in = CharField(max_length=50); 
+	refresh_token = CharField(max_length=50);
+	scope = CharField(max_length = 50);
+	state = CharFIeld(max_length = 50);
+	 
+
+
